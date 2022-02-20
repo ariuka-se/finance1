4b1cd17de0bf73b1908a8db71cf8b96dd7362259
nnasaa88 <nasaa88@gmail.com>
2022-02-20 18:41:01 +0800

    ӨӨрядөд ийош
    товч болог моуз өөрчлөлт

--- app.js
+++ app.js
@@ -1,40 +1,27 @@
-var uiController = (function() {
-    var x = 100;
-  
-    function add(y) {
-      return x + y;
+// Ð”ÑÐ»Ð³ÑÑ†Ñ‚ÑÐ¹ Ð°Ð¶Ð¸Ð»Ð»Ð°Ñ… ÐºÐ¾Ð½Ñ‚Ñ€Ð¾Ð»Ð»ÐµÑ€
+var uiController = (function() {})();
+
+// Ð¡Ð°Ð½Ñ…Ò¯Ò¯Ñ‚ÑÐ¹ Ð°Ð¶Ð¸Ð»Ð»Ð°Ñ… ÐºÐ¾Ð½Ñ‚Ñ€Ð¾Ð»Ð»ÐµÑ€
+var financeController = (function() {})();
+
+// ÐŸÑ€Ð¾Ð³Ñ€Ð°Ð¼Ñ‹Ð½ Ñ…Ð¾Ð»Ð±Ð¾Ð³Ñ‡ ÐºÐ¾Ð½Ñ‚Ñ€Ð¾Ð»Ð»ÐµÑ€
+var appController = (function(uiController, financeController) {
+  var ctrlAddItem = function() {
+    // 1. ÐžÑ€ÑƒÑƒÐ»Ð°Ñ… Ó©Ð³Ó©Ð³Ð´Ð»Ð¸Ð¹Ð³ Ð´ÑÐ»Ð³ÑÑ†ÑÑÑ Ð¾Ð»Ð¶ Ð°Ð²Ð½Ð°.
+    console.log("Ð”ÑÐ»Ð³ÑÑ†ÑÑÑ Ó©Ð³Ó©Ð³Ð´Ð»Ó©Ó© Ð°Ð²Ð°Ñ… Ñ…ÑÑÑÐ³");
+    // 2. ÐžÐ»Ð¶ Ð°Ð²ÑÐ°Ð½ Ó©Ð³Ó©Ð³Ð´Ð»Ò¯Ò¯Ð´ÑÑ ÑÐ°Ð½Ñ…Ò¯Ò¯Ð³Ð¸Ð¹Ð½ ÐºÐ¾Ð½Ñ‚Ñ€Ð¾Ð»Ð»ÐµÑ€Ñ‚ Ð´Ð°Ð¼Ð¶ÑƒÑƒÐ»Ð¶ Ñ‚ÑÐ½Ð´ Ñ…Ð°Ð´Ð³Ð°Ð»Ð½Ð°.
+    // 3. ÐžÐ»Ð¶ Ð°Ð²ÑÐ°Ð½ Ó©Ð³Ó©Ð³Ð´Ð»Ò¯Ò¯Ð´ÑÑ Ð²ÑÐ± Ð´ÑÑÑ€ÑÑ Ñ‚Ð¾Ñ…Ð¸Ñ€Ð¾Ñ… Ñ…ÑÑÑÐ³Ñ‚ Ð½ÑŒ Ð³Ð°Ñ€Ð³Ð°Ð½Ð°
+    // 4. Ð¢Ó©ÑÐ²Ð¸Ð¹Ð³ Ñ‚Ð¾Ð¾Ñ†Ð¾Ð¾Ð»Ð½Ð¾
+    // 5. Ð­Ñ†ÑÐ¸Ð¹Ð½ Ò¯Ð»Ð´ÑÐ³Ð´ÑÐ», Ñ‚Ð¾Ð¾Ñ†Ð¾Ð¾Ð³ Ð´ÑÐ»Ð³ÑÑ†ÑÐ½Ð´ Ð³Ð°Ñ€Ð³Ð°Ð½Ð°.
+  };
+
+  document.querySelector(".add__btn").addEventListener("click", function() {
+    ctrlAddItem();
+  });
+
+  document.addEventListener("keypress", function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
     }
-  
-    return {
-      publicAdd: function(a) {
-        a = add(a);
-        console.log("Боловсруулсан утга : " + a);
-      }
-    };
-  })();
-  
-  var financeController = (function() {})();
-  
-  var appController = (function(uiController, financeController) {
-    uiController.publicAdd(150);
-  })(uiController, financeController);
-  
-  // var hunController = (function() {
-  
-  //     // data encapsulation
-  
-  //     // private data
-  //   var bodol = "Javascript толгой эргүүлмээр юм...";
-  //   function tsusGuih() {}
-  //     // private function
-  //   function huchilturugchiigAgaaraasSorjTsusruuOruulah() {}
-  
-  //   return {
-  //     yarih: function() {
-  //       bodol = "Javascript бол лаг";
-  //       huchilturugchiigAgaaraasSorjTsusruuOruulah();
-  //       tsusGuih();
-  //       console.log("hi");
-  //     }
-  //   };
-  // })();
+  });
+})(uiController, financeController);
